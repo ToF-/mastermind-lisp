500b80d06cdd79ee58f40945f3e7822d07b871b7
Christophe Thibaut <cthibauttof@gmail.com>
2026-03-12 08:38:34 +0100

    added a command line with optional secret code

---
+++ mm
@@ -0,0 +1,2 @@
+#! /bin/bash
+sbcl --noinform --load ~/.sbclrc --script src/mm.lisp $1
--- src/mm.lisp
+++ src/mm.lisp
@@ -2,6 +2,14 @@
 
 (load "src/mastermind")
 
-(let ((rs (make-random-state t)))
-  (guess-secret (number-to-key (random (expt *max-colors* *max-pegs*) rs))))
+(defun main ()
+  (let ((args sb-ext:*posix-argv*))
+    (progn 
+      (format t "~A~%" args)
+      (if (eq 2 (length args))
+        (guess-secret (parse-integer (cadr args)))
+        (let ((rs (make-random-state t)))
+          (guess-secret (number-to-key (random (expt *max-colors* *max-pegs*) rs))))))))
+
+(main)
 (quit)
